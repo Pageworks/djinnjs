9b865798a4e99429c76bf823decb55142329ce3a
Kyle Andrews <kylea@page.works>
2020-01-13 10:57:39 -0500

    Added none scroll behavior option to the None transition

--- src/transitions/none.ts
+++ src/transitions/none.ts
@@ -1,17 +1,19 @@
 export function none(selector: string, newHTML: string, target: HTMLElement | null): Promise<{}> {
     return new Promise(resolve => {
-        let behavior: ScrollBehavior = "auto";
+        let behavior: ScrollBehavior | "none" = "auto";
         if (target) {
             const desiredBehavior = target.getAttribute("scroll");
-            if (desiredBehavior === "auto" || desiredBehavior === "smooth") {
+            if (desiredBehavior === "auto" || desiredBehavior === "smooth" || desiredBehavior === "none") {
                 behavior = desiredBehavior;
             }
         }
-        window.scroll({
-            top: 0,
-            left: 0,
-            behavior: behavior,
-        });
+        if (behavior !== "none") {
+            window.scroll({
+                top: 0,
+                left: 0,
+                behavior: behavior,
+            });
+        }
         const view = document.body.querySelector(selector) as HTMLElement;
         view.innerHTML = newHTML;
         resolve();
